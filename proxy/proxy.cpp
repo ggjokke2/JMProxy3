b4b305aac583931a9cecd645388a2468a1de525f
ggjokke2 <45535502+ggjokke2@users.noreply.github.com>
2021-10-09 23:49:59 +0300

    Add files via upload

---
+++ proxy/proxy.cpp
@@ -0,0 +1,32 @@
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <thread>
+#include "enet/include/enet.h"
+#include "http.h"
+#include "server.h"
+
+server* g_server = new server();
+
+int main() {
+#ifdef _WIN32
+    SetConsoleTitleA("JMProxy");
+#endif
+    printf("JMProxy by Joakim Meriko\n");
+
+    std::thread http(http::run, "127.0.0.1", "17191");
+    http.detach();
+    printf("HTTP server is running.\n");
+
+    enet_initialize();
+    if (g_server->start()) {
+        printf("Server & client proxy is running.\n");
+        while (true) {
+            g_server->poll();
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+    }   
+    else
+        printf("Failed to start server or proxy.\n");
+}
